3c9629f1af4bc49a1feb274e7135e573868ca09b
berkguzel <berkguzeell@gmail.com>
2020-10-27 22:22:21 +0300

    added status argument

--- catch-container.sh
+++ catch-container.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
 dateDiff () {
-	date=$( date -d "$1" +%s)
-	containerDate=$( date -d "$2" +%s)
+	date=$( date -d "$1" +%s )
+	containerDate=$( date -d "$2" +%s )
 	echo "$(( (date - containerDate) / 86400))"
 }
+
+for argument in "$@"; do
+   key=$( echo "${argument}" | cut -d '=' -f 1 )
+   value=$( echo "${argument}" | cut -d '=' -f 2 )
+   case $key in
+		"--help" | "-h")
+			echo " --status, -s = all, exited, created, running\n --time, -t = h(hour), d(day), m(month) " 
+			;;
+		"--status" | "-s")
+			status="$value"
+			;;
+		"--time" | "-t")
+			timeRange="$value"
+			;;
+	esac
+done
+			
 
 docker ps -a | awk '{ print $1 }' | \
 while read containerId; do
   if [ "$containerId" != "CONTAINER" ]; then
-  	containerDate=$( docker inspect $containerId | grep "FinishedAt" | awk '{ print $2 }' | cut -c 2-11 )
-	yourTime=$( date +"%Y-%m-%-d" ) 
-	diff=$(dateDiff "$yourTime" "$containerDate")
+  	yourTime=$( date +"%Y-%m-%-d" )
+	containerStatus=$( docker inspect -f '{{.State.Status}}' $containerId )
+	if [ "$status" = "running" ]; then
+		containerDate=$( docker inspect $containerId | grep "StartedAt" | awk '{ print $2 }' | cut -c 2-11 )
+	fi
+	if [ "$status" = "created" ]; then
+		containerDate=$( docker inspect -f '{{.Created}}' $containerId )
 	
-	if [ "$diff" -eq "0" ]; then
-		huntedContainerId="$( docker rm "$containerId" )"
-		echo "$huntedContainerId deleted"
+	fi
+	if [ "$status" = "exited" ]; then 
+		containerDate=$( docker inspect $containerId | grep "FinishedAt" | awk '{ print $2 }' | cut -c 2-11 )
+	fi
+	
+	diff=$( dateDiff "$yourTime" "$containerDate" )
+	if [ "$diff" -eq "0" ] && [ "$containerStatus" = "$status" ]; then
+			catchedContainerId="$( docker rm -f "$containerId" )"
+			echo "$catchedContainerId deleted"
 	fi
   fi
 done
