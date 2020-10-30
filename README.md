8a6a28dfeb1c7e72af6aeb658170735040410d96
berkguzel <berkguzeell@gmail.com>
2020-10-30 20:55:55 +0300

    added day argument

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # container-catcher
-script for deleting exited containers
+script for deleting containers by day, hours
+
+# USAGE
+
+1. `git clone https://github.com/berkguzel/container-catcher.git'
+2. Go into to directory and run `sh catch-container.sh --status= --time `
--- catch-container.sh
+++ catch-container.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 dateDiff () {
+
 	date=$( date -d "$1" +%s )
-	containerDate=$( date -d "$2" +%s )
+	containerDate=$( date -d "$3" +%s )
 	echo "$(( (date - containerDate) / 86400))"
+
 }
 
 for argument in "$@"; do
-   key=$( echo "${argument}" | cut -d '=' -f 1 )
-   value=$( echo "${argument}" | cut -d '=' -f 2 )
-   case $key in
+	key=$( echo "${argument}" | cut -d '=' -f 1 )
+	value=$( echo "${argument}" | cut -d '=' -f 2 )
+	case $key in
 		"--help" | "-h")
 			echo " --status, -s = all, exited, created, running\n --time, -t = h(hour), d(day), m(month) " 
 			;;
@@ -17,7 +19,8 @@ for argument in "$@"; do
 			status="$value"
 			;;
 		"--time" | "-t")
-			timeRange="$value"
+			timeRange="$( echo  ${value%?} )"
+			timeChoice="$( echo -n $value | tail -c 1 )"
 			;;
 	esac
 done
@@ -28,21 +31,21 @@ while read containerId; do
   if [ "$containerId" != "CONTAINER" ]; then
   	yourTime=$( date +"%Y-%m-%-d" )
 	containerStatus=$( docker inspect -f '{{.State.Status}}' $containerId )
+
 	if [ "$status" = "running" ]; then
 		containerDate=$( docker inspect $containerId | grep "StartedAt" | awk '{ print $2 }' | cut -c 2-11 )
-	fi
-	if [ "$status" = "created" ]; then
-		containerDate=$( docker inspect -f '{{.Created}}' $containerId )
+
+	elif [ "$status" = "created" ]; then
+		containerDate=$( docker inspect -f '{{.Created}}' $containerId )	
 	
+	elif [ "$status" = "exited" ]; then 
+		containerDate=$( docker inspect $containerId | grep "FinishedAt" | awk '{ print $2 }' | cut -c 2-11 )	
 	fi
-	if [ "$status" = "exited" ]; then 
-		containerDate=$( docker inspect $containerId | grep "FinishedAt" | awk '{ print $2 }' | cut -c 2-11 )
-	fi
-	
-	diff=$( dateDiff "$yourTime" "$containerDate" )
-	if [ "$diff" -eq "0" ] && [ "$containerStatus" = "$status" ]; then
-			catchedContainerId="$( docker rm -f "$containerId" )"
-			echo "$catchedContainerId deleted"
+
+	diff=$( dateDiff "$yourTime" "$containerDate" "$timeChoice" )
+	if [ "$diff" -le "$timeRange" ] && [ "$containerStatus" = "$status" ]; then
+		catchedContainerId="$( docker rm -f "$containerId" )"
+		echo "$catchedContainerId deleted"
 	fi
   fi
 done
